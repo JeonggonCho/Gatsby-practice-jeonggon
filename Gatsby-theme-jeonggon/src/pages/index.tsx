fac56aff7f0b30e6617e51df8934263d184a5f60
조정곤 <wjdrhs62@gmail.com>
2024-04-19 18:26:30 +0900

    add: Template component

--- Gatsby-theme-jeonggon/src/pages/index.tsx
+++ Gatsby-theme-jeonggon/src/pages/index.tsx
@@ -1,14 +1,13 @@
 import React, { FunctionComponent, useMemo } from 'react'
 import styled from '@emotion/styled'
-import GlobalStyle from 'components/Common/GlobalStyle'
 import Introduction from 'components/Main/Introduction'
 import CategoryList from 'components/Main/CategoryList'
-import Footer from 'components/Common/Footer'
 import PostList from 'components/Main/PostList'
 import {graphql} from 'gatsby'
 import { PostListItemType } from '../types/PostItem.types'
 import {IGatsbyImageData} from 'gatsby-plugin-image'
 import queryString, {ParsedQuery} from 'query-string'
+import Template from 'components/Common/Template'
 
 type IndexPageProps = {
   location: {
@@ -25,12 +24,6 @@ type IndexPageProps = {
     }
   }
 }
-
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-`;
 
 const IndexPage: FunctionComponent<IndexPageProps> = function ({
   location: {search},
@@ -71,17 +64,15 @@ const IndexPage: FunctionComponent<IndexPageProps> = function ({
   )
 
   return (
-    <Container>
-      <GlobalStyle />
+    <Template>
       <Introduction profileImage={gatsbyImageData} />
       <CategoryList selectedCategory={selectedCategory} categoryList={categoryList} />
       <PostList selectedCategory={selectedCategory} posts={edges} />
-      <Footer />
-    </Container>
+    </Template>
   )
-}
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const getPostList = graphql`
     query getPostList {
@@ -114,4 +105,4 @@ export const getPostList = graphql`
             }
         }
     }
-`
+`;
--- Gatsby-theme-jeonggon/src/templates/post_template.tsx
+++ Gatsby-theme-jeonggon/src/templates/post_template.tsx
@@ -1,11 +1,38 @@
 import React, { FunctionComponent } from 'react'
+import { graphql } from 'gatsby'
+import Template from 'components/Common/Template'
 
 type PostTemplateProps = {};
 
 const PostTemplate: FunctionComponent<PostTemplateProps> = function (props) {
   console.log(props);
 
-  return <div>Post Template</div>;
+  return (
+    <Template>Post Template</Template>
+  );
 };
 
 export default PostTemplate;
+
+export const queryMarkdownDataSlug = graphql`
+    query queryMarkdownDataBySlug($slug: String) {
+        allMarkdownRemark(filter: {fields: {slug: {eq: $slug}}}) {
+            edges {
+                node {
+                    html
+                    frontmatter {
+                        title
+                        summary
+                        date(formatString: "YYYY.MM.DD.")
+                        categories
+                        thumbnail {
+                            childImageSharp {
+                                gatsbyImageData
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
