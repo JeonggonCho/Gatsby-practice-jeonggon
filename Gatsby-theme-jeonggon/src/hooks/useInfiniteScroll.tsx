1cdafd2ae196278ff27368cbdf5f61fff02ade65
조정곤 <wjdrhs62@gmail.com>
2024-04-21 01:48:48 +0900

    add: sitemap

--- Gatsby-theme-jeonggon/src/hooks/useInfiniteScroll.tsx
+++ Gatsby-theme-jeonggon/src/hooks/useInfiniteScroll.tsx
@@ -13,30 +13,40 @@ const useInfiniteScroll = function (
   posts: PostListItemType[],
 ): useInfiniteScrollType {
 
-  const containerRef: MutableRefObject<HTMLDivElement | null> = useRef<HTMLDivElement>(
-    null,
-  );
+  const containerRef: MutableRefObject<HTMLDivElement | null> = useRef<HTMLDivElement>(null);
+
+  const observer: MutableRefObject<IntersectionObserver | null> = useRef<IntersectionObserver>(null);
 
   const [count, setCount] = useState<number>(1);
 
-  const postListByCategory = useMemo(() =>
+  const postListByCategory = useMemo<PostListItemType[]>(
+    () =>
     posts.filter(({node: {frontmatter: {categories}}}: PostListItemType) =>
       selectedCategory !== 'All' ? categories.includes(selectedCategory): true,
     ), [selectedCategory]);
 
-  const observer: IntersectionObserver = new IntersectionObserver((entries, observer) => {
-    if (!entries[0].isIntersecting) return;
-    setCount(value => value + 1);
-    observer.disconnect();
-    }
-  );
+  useEffect(() => {
+    observer.current = new IntersectionObserver((entries, observer) => {
+      if (!entries[0].isIntersecting) return;
 
-  useEffect(() => setCount(1), [selectedCategory]);
+      setCount(value => value + 1);
+      observer.unobserve(entries[0].target);
+    })
+  }, [])
 
   useEffect(() => {
-    if (NUMBER_OF_ITEMS_PER_PAGE * count >= postListByCategory.length || containerRef.current === null || containerRef.current.children.length === 0) return;
-    observer.observe(containerRef.current.children[containerRef.current.children.length - 1]);
-  }, [count, selectedCategory]);
+    if (
+      NUMBER_OF_ITEMS_PER_PAGE * count >= postListByCategory.length ||
+      containerRef.current === null ||
+      containerRef.current.children.length === 0 ||
+      observer.current === null
+    )
+      return;
+
+    observer.current.observe(
+      containerRef.current.children[containerRef.current.children.length - 1],
+    )
+  }, [count, selectedCategory])
 
   return {
     containerRef,
@@ -44,4 +54,4 @@ const useInfiniteScroll = function (
   }
 }
 
-export default useInfiniteScroll;
+export default useInfiniteScroll
