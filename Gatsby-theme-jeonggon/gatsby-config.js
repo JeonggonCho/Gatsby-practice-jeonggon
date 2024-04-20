ddeb647c8a304adea46be6dab849018929305124
조정곤 <wjdrhs62@gmail.com>
2024-04-21 00:57:40 +0900

    add: gatsby-plugin-canonical-urls

--- Gatsby-theme-jeonggon/gatsby-config.js
+++ Gatsby-theme-jeonggon/gatsby-config.js
@@ -7,6 +7,13 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: 'gatsby-plugin-canonical-urls',
+      options: {
+        siteUrl: 'https://naver.com',
+        stripQueryString: true,
+      }
+    },
+    {
       resolve: 'gatsby-plugin-typescript',
       options: {
         isTSX: true,
